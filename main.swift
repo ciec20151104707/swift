c9f8d43e37e5c97319927ebf4633342aad0f7398
liyi <826655265@qq.com>
2017-02-27 11:44:20 +0800

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  累加
+//
+//  Created by s20151104707 on 17/2/27.
+//  Copyright © 2017年 s20151104707. All rights reserved.
+//
+
+import Foundation
+
+var sum = 0
+
+for i in 1...100{
+    sum += i
+}
+print(sum)
+
+
+
+
